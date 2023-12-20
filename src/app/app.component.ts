d3988d172f123f4ef27ca9f62bde2e2e18f860b4
grz55 <grz55@vp.pl>
2023-12-21 00:39:55 +0100

    feat(11): exercise added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ExerciseComponent } from './exercise/exercise.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ExerciseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
